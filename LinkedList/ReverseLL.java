31cf052abeebd57142e9f11856b08e372dcc9f25
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-05-09 09:15:09 +0530

    09-05-2026

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -2,6 +2,16 @@ package LinkedList;
 
 public class ReverseLL {
     public void reverse(LinkedList ll){
-       
+        Node prev = null;
+        Node curr = ll.head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next; // store next node
+            curr.next = prev; // reverse the link
+            prev = curr; // move prev to current
+            curr = next; // move to next node
+        }
+        ll.head = prev; // update head to new first node
     }
 }
