391b0a855ff3c2f5852965ed387e22ac48856346
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-05-10 09:16:26 +0530

    10-05-2026

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -16,3 +16,57 @@ public class ReverseLL {
     }
 }
 
+
+public class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        
+        System.out.println("Original List:");
+        ll.printList();
+
+        ReverseLL reverser = new ReverseLL();
+        reverser.reverse(ll);
+
+        System.out.println("Reversed List:");
+        ll.printList();
+    }
+}
+
