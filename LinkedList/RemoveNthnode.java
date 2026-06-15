ceb671d60b0c26fded450cd162c904a5f4b9cda0
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-06-15 11:20:02 +0530

    15-06-2026

--- LinkedList/RemoveNthnode.java
+++ LinkedList/RemoveNthnode.java
@@ -28,3 +28,83 @@ public class RemoveNthnode {
 }
 
 
+public class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    public LinkedList() {
+        this.size = 0;
+    }
+
+    private class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private int helper(Node head, int key){
+        if(head == null){
+            return 0;
+        }
+        int count = helper(head.next, key) + 1;
+        if(count == key){
+            head.next = head.next.next;
+        }
+        return count;
+    }
+
+    public void removeNthFromEnd(int n){
+        helper(head, n);
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        System.out.println("Original List:");
+        ll.printList();
+
+        int n = 2;
+        ll.removeNthFromEnd(n);
+        System.out.println("List after removing " + n + "th node from the end:");
+        ll.printList();
+    }
+}
